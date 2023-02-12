4cc91a52895bc990425f35978e3ca85e4acaf0a7
Vernon Miller <vernon.w.miller@gmail.com>
2023-02-12 11:27:16 -0700

    triangle calculations and code cleanup

--- geometry.go
+++ geometry.go
@@ -73,6 +73,85 @@ func (p Point) Draw(s float64, ctx *canvas.Context) {
 	ctx.DrawPath(p.X, p.Y, canvas.Circle(s))
 }
 
+// Scale multiplies point coordinates by a fixed value
+func (p Point) Scale(x, y float64) Point {
+	return Point{X: p.X * x, Y: p.Y * y}
+}
+
+// ScaleX scales the x value of a point
+func (p Point) ScaleX(x float64) Point {
+	return Point{X: p.X * x, Y: p.Y}
+}
+
+// ScaleY scales the y value of a point
+func (p Point) ScaleY(y float64) Point {
+	return Point{X: p.X, Y: p.Y * y}
+}
+
+// Rotate calculates a new point rotated around the origin by a given angle
+func (p Point) Rotate(a float64) Point {
+	x := p.X*math.Cos(a) - p.Y*math.Sin(a)
+	y := p.Y*math.Sin(a) + p.Y*math.Cos(a)
+	return Point{X: x, Y: y}
+}
+
+// Shear calculates a new point sheared given angles for the x and y directions
+func (p Point) Shear(x, y float64) Point {
+	newX := p.X + p.Y*math.Tan(x)
+	newY := p.X*math.Tan(y) + p.Y
+	return Point{X: newX, Y: newY}
+}
+
+// ShearX calculates a new point sheared in the x direction by a given angle
+func (p Point) ShearX(a float64) Point {
+	return Point{X: p.X + p.Y*math.Tan(a), Y: p.Y}
+}
+
+// ShearY calculates a new point sheared in the x direction by a given angle
+func (p Point) ShearY(a float64) Point {
+	return Point{X: p.X, Y: p.X*math.Tan(a) + p.Y}
+}
+
+// Reflect calculates a new point that is reflected about both the x and y axes
+func (p Point) Reflect() Point {
+	return p.Scale(-1, -1)
+}
+
+// ReflectX calculates a new point that is reflected about the x-axis
+func (p Point) ReflectX() Point {
+	return p.ScaleX(-1)
+}
+
+// ReflectY calculates a new point that is reflected about the y-axis
+func (p Point) ReflectY() Point {
+	return p.ScaleY(-1)
+}
+
+// Translate calculates a new point with coordinates translated by the given amounts
+func (p Point) Translate(x, y float64) Point {
+	return Point{X: p.X + x, Y: p.Y + y}
+}
+
+// TranslateX calculates a new point with the x coordinated translated by the given amount
+func (p Point) TranslateX(x float64) Point {
+	return Point{X: p.X + x, Y: p.Y}
+}
+
+// TranslateY calculates a new point with the y coordinated translated by the given amount
+func (p Point) TranslateY(y float64) Point {
+	return Point{X: p.X, Y: p.Y + y}
+}
+
+// Copy returns a new point with the same x and y coordinates
+func (p Point) Copy() Point {
+	return Point{X: p.X, Y: p.Y}
+}
+
+// ToVec2 translates the point to a Vec2 struct
+func (p Point) ToVec2() Vec2 {
+	return Vec2{X: p.X, Y: p.Y}
+}
+
 // Distance between two points
 func Distance(p Point, q Point) float64 {
 	return math.Sqrt(math.Pow(q.X-p.X, 2) + math.Pow(q.Y-p.Y, 2))
@@ -81,89 +160,6 @@ func Distance(p Point, q Point) float64 {
 // SquaredDistance is the square of the distance between two points
 func SquaredDistance(p Point, q Point) float64 {
 	return math.Pow(q.X-p.X, 2) + math.Pow(q.Y-p.Y, 2)
-}
-
-// Scale multiplies point coordinates by a fixed value
-func (p *Point) Scale(x, y float64) {
-	p.X *= x
-	p.Y *= y
-}
-
-// ScaleX scales the x value of a point
-func (p *Point) ScaleX(x float64) {
-	p.X *= x
-}
-
-// ScaleY scales the y value of a point
-func (p *Point) ScaleY(y float64) {
-	p.Y *= y
-}
-
-// Rotate calculates a new point rotated around the origin by a given angle
-func (p *Point) Rotate(a float64) {
-	x := p.X*math.Cos(a) - p.Y*math.Sin(a)
-	y := p.Y*math.Sin(a) + p.Y*math.Cos(a)
-	p.X = x
-	p.Y = y
-}
-
-// Shear calculates a new point sheared given angles for the x and y directions
-func (p *Point) Shear(x, y float64) {
-	newX := p.X + p.Y*math.Tan(x)
-	newY := p.X*math.Tan(y) + p.Y
-	p.X = newX
-	p.Y = newY
-}
-
-// ShearX calculates a new point sheared in the x direction by a given angle
-func (p *Point) ShearX(a float64) {
-	p.X = p.X + p.Y*math.Tan(a)
-}
-
-// ShearY calculates a new point sheared in the x direction by a given angle
-func (p *Point) ShearY(a float64) {
-	p.Y = p.X*math.Tan(a) + p.Y
-}
-
-// Reflect calculates a new point that is reflected about both the x and y axes
-func (p *Point) Reflect() {
-	p.Scale(-1, -1)
-}
-
-// ReflectX calculates a new point that is reflected about the x-axis
-func (p *Point) ReflectX() {
-	p.ScaleX(-1)
-}
-
-// ReflectY calculates a new point that is reflected about the y-axis
-func (p *Point) ReflectY() {
-	p.ScaleY(-1)
-}
-
-// Translate calculates a new point with coordinates translated by the given amounts
-func (p *Point) Translate(x, y float64) {
-	p.X += x
-	p.Y += y
-}
-
-// TranslateX calculates a new point with the x coordinated translated by the given amount
-func (p *Point) TranslateX(x float64) {
-	p.X += x
-}
-
-// TranslateY calculates a new point with the y coordinated translated by the given amount
-func (p *Point) TranslateY(y float64) {
-	p.Y += y
-}
-
-// Copy returns a new point with the same x and y coordinates
-func (p *Point) Copy() Point {
-	return Point{X: p.X, Y: p.Y}
-}
-
-// ToVec2 translates the point to a Vec2 struct
-func (p *Point) ToVec2() Vec2 {
-	return Vec2{X: p.X, Y: p.Y}
 }
 
 // Line functions
@@ -303,36 +299,44 @@ func (l Line) ParallelTo(k Line) bool {
 }
 
 // Scale calculates a new line for which both points are scaled by the given amount
-func (l *Line) Scale(x, y float64) {
-	l.P.Scale(x, y)
-	l.Q.Scale(x, y)
+func (l Line) Scale(x, y float64) Line {
+	return Line{
+		P: l.P.Scale(x, y),
+		Q: l.Q.Scale(x, y),
+	}
 }
 
 // Rotate calculates a new line for which both points are rotated by the given angle
-func (l *Line) Rotate(a float64) {
-	l.P.Rotate(a)
-	l.Q.Rotate(a)
+func (l Line) Rotate(a float64) Line {
+	return Line{
+		P: l.P.Rotate(a),
+		Q: l.Q.Rotate(a),
+	}
 }
 
 // Shear calculates a new line for which both points are sheared by the given amount
-func (l *Line) Shear(x, y float64) {
-	l.P.Shear(x, y)
-	l.Q.Shear(x, y)
+func (l Line) Shear(x, y float64) Line {
+	return Line{
+		P: l.P.Shear(x, y),
+		Q: l.Q.Shear(x, y),
+	}
 }
 
 // Translate calculates a new line for which both points are translated by the given amount
-func (l *Line) Translate(x, y float64) {
-	l.P.Translate(x, y)
-	l.Q.Translate(x, y)
+func (l Line) Translate(x, y float64) Line {
+	return Line{
+		P: l.P.Translate(x, y),
+		Q: l.Q.Translate(x, y),
+	}
 }
 
 // Copy returns a new line with the same points P and Q
-func (l *Line) Copy() Line {
+func (l Line) Copy() Line {
 	return Line{P: l.P.Copy(), Q: l.Q.Copy()}
 }
 
 // Boundary returns the smallest rect that contains both points in the line
-func (l *Line) Boundary() Rect {
+func (l Line) Boundary() Rect {
 	minX := math.Min(l.P.X, l.Q.X)
 	minY := math.Min(l.P.Y, l.Q.Y)
 	maxX := math.Max(l.P.X, l.Q.X)
@@ -585,12 +589,12 @@ func (c *Curve) Boundary() Rect {
 // Circle functions
 
 // Draw draws the circle given a canvas context
-func (c *Circle) Draw(ctx *canvas.Context) {
+func (c Circle) Draw(ctx *canvas.Context) {
 	ctx.DrawPath(c.Center.X, c.Center.Y, canvas.Circle(c.Radius))
 }
 
 // ToCurve calculates a curve that approximates the circle with a given resolution (number of sides)
-func (c *Circle) ToCurve(resolution int) Curve {
+func (c Circle) ToCurve(resolution int) Curve {
 	points := make([]Point, resolution)
 	theta := Linspace(0, Tau, resolution, false)
 	for i, t := range theta {
@@ -602,17 +606,17 @@ func (c *Circle) ToCurve(resolution int) Curve {
 }
 
 // ContainsPoint determines if a point lies inside the circle, including the boundary
-func (c *Circle) ContainsPoint(p Point) bool {
+func (c Circle) ContainsPoint(p Point) bool {
 	return Distance(c.Center, p) <= c.Radius
 }
 
 // PointOnEdge determines if a point lies on the boundary of a circle
-func (c *Circle) PointOnEdge(p Point) bool {
+func (c Circle) PointOnEdge(p Point) bool {
 	return Equalf(Distance(c.Center, p), c.Radius)
 }
 
 // Copy returns a new circle with the same center and radius
-func (c *Circle) Copy() Circle {
+func (c Circle) Copy() Circle {
 	return Circle{
 		Center: c.Center.Copy(),
 		Radius: c.Radius,
@@ -620,7 +624,7 @@ func (c *Circle) Copy() Circle {
 }
 
 // Boundary returns the smallest rect that contains all points on the circle
-func (c *Circle) Boundary() Rect {
+func (c Circle) Boundary() Rect {
 	x := c.Center.X
 	y := c.Center.Y
 	r := c.Radius
@@ -632,12 +636,12 @@ func (c *Circle) Boundary() Rect {
 // Rect functions
 
 // ContainsPoint determines if a point lies within a rectangle
-func (r *Rect) ContainsPoint(p Point) bool {
+func (r Rect) ContainsPoint(p Point) bool {
 	return p.X >= r.X && p.X <= r.X+r.W && p.Y >= r.Y && p.Y <= r.Y+r.H
 }
 
 // Contains determines if the rectangle contains a given rectangle
-func (r *Rect) Contains(rect Rect) bool {
+func (r Rect) Contains(rect Rect) bool {
 	a := Point{X: r.X, Y: r.Y}
 	b := Point{X: r.X + r.W, Y: r.Y + r.H}
 	c := Point{X: rect.X, Y: rect.Y}
@@ -646,7 +650,7 @@ func (r *Rect) Contains(rect Rect) bool {
 }
 
 // IsDisjoint determines if the rectangle is disjoint (no overlap) from a given rectangle
-func (r *Rect) IsDisjoint(rect Rect) bool {
+func (r Rect) IsDisjoint(rect Rect) bool {
 	aLeft := r.X
 	aRight := r.X + r.W
 	aTop := r.Y + r.H
@@ -663,12 +667,12 @@ func (r *Rect) IsDisjoint(rect Rect) bool {
 }
 
 // Overlaps determines if the rectangle overlaps the given rectangle
-func (r *Rect) Overlaps(rect Rect) bool {
+func (r Rect) Overlaps(rect Rect) bool {
 	return !r.IsDisjoint(rect)
 }
 
 // Intersects determines if the rectangle intersects the given rectangle
-func (r *Rect) Intersects(rect Rect) bool {
+func (r Rect) Intersects(rect Rect) bool {
 	a := Point{X: r.X, Y: r.Y}
 	b := Point{X: r.X + r.W, Y: r.Y + r.H}
 	c := Point{X: rect.X, Y: rect.Y}
@@ -686,7 +690,7 @@ func (r *Rect) Intersects(rect Rect) bool {
 }
 
 // ToCurve calculates a closed curve with points corresponding to the vertices of the rectangle
-func (r *Rect) ToCurve() Curve {
+func (r Rect) ToCurve() Curve {
 	var curve Curve
 	curve.Closed = true
 	curve.Points = append(curve.Points, Point{X: r.X, Y: r.Y})
@@ -697,25 +701,25 @@ func (r *Rect) ToCurve() Curve {
 }
 
 // Draw draws the rectangle given a canvas context
-func (r *Rect) Draw(ctx *canvas.Context) {
+func (r Rect) Draw(ctx *canvas.Context) {
 	rect := canvas.Rectangle(r.W, r.H)
 	ctx.DrawPath(r.X, r.Y, rect)
 }
 
 // Copy returns a new rectangle with the same corner, width, and height
-func (r *Rect) Copy() Rect {
+func (r Rect) Copy() Rect {
 	return Rect{X: r.X, Y: r.Y, W: r.W, H: r.H}
 }
 
 // Center returns a point at the center of the rectangle
-func (r *Rect) Center() Point {
+func (r Rect) Center() Point {
 	return Point{X: r.X + 0.5*r.W, Y: r.Y + 0.5*r.H}
 }
 
 // Triangle functions
 
 // ToCurve calculates a closed curve with points corresponding to the vertices of the triangle
-func (t *Triangle) ToCurve() Curve {
+func (t Triangle) ToCurve() Curve {
 	return Curve{
 		Points: []Point{t.A, t.B, t.C},
 		Closed: true,
@@ -723,7 +727,7 @@ func (t *Triangle) ToCurve() Curve {
 }
 
 // Draw draws the triangle given a canvas context
-func (t *Triangle) Draw(ctx *canvas.Context) {
+func (t Triangle) Draw(ctx *canvas.Context) {
 	ctx.MoveTo(t.A.X, t.A.Y)
 	ctx.LineTo(t.B.X, t.B.Y)
 	ctx.LineTo(t.C.X, t.C.Y)
@@ -731,7 +735,7 @@ func (t *Triangle) Draw(ctx *canvas.Context) {
 }
 
 // Area calculates the area of the triangle
-func (t *Triangle) Area() float64 {
+func (t Triangle) Area() float64 {
 	// Heron's formula
 	a := Line{P: t.A, Q: t.B}.Length()
 	b := Line{P: t.B, Q: t.C}.Length()
@@ -741,22 +745,133 @@ func (t *Triangle) Area() float64 {
 }
 
 // Perimeter calculates the perimeter of the triangle
-func (t *Triangle) Perimeter() float64 {
+func (t Triangle) Perimeter() float64 {
 	a := Line{P: t.A, Q: t.B}.Length()
 	b := Line{P: t.B, Q: t.C}.Length()
 	c := Line{P: t.C, Q: t.A}.Length()
 	return a + b + c
 }
 
+// AB returns the length of the side defined by vertices A and B
+// also known as side "c" in typical notation
+func (t Triangle) AB() float64 {
+	return Distance(t.A, t.B)
+}
+
+// AC returns the length of the side defined by vertices A and C
+// also known as side "b" in typical notation
+func (t Triangle) AC() float64 {
+	return Distance(t.A, t.C)
+}
+
+// BC returns the length of the side defined by vertices B and C
+// also known as side "a" in typical notation
+func (t Triangle) BC() float64 {
+	return Distance(t.B, t.C)
+}
+
+// Angles calculates the interior angles of the triangle
+func (t Triangle) Angles() (float64, float64, float64) {
+	a := t.BC()
+	b := t.AC()
+	c := t.AB()
+	alpha := math.Acos((b*b + c*c - a*a) / (2 * b * c))
+	beta := math.Acos((a*a + c*c - b*b) / (2 * a * c))
+	gamma := math.Acos((a*a + b*b - c*c) / (2 * a * b))
+	return alpha, beta, gamma
+}
+
+// IncircleRadius calculates the radius of the incircle
+func (t Triangle) IncircleRadius() float64 {
+	return 2 * t.Area() / t.Perimeter()
+}
+
+// CircumcircleRadius calculates the radius of the circumcircle
+func (t Triangle) CircumcircleRadius() float64 {
+	return (t.AB() * t.BC() * t.AC()) / (4 * t.Area())
+}
+
+// NinePointRadius calculates the radius of the nine-point circle
+func (t Triangle) NinePointRadius() float64 {
+	return t.CircumcircleRadius() / 2
+}
+
 // Centroid calculates the centroid of the triangle
-func (t *Triangle) Centroid() Point {
+func (t Triangle) Centroid() Point {
 	x := (t.A.X + t.B.X + t.C.X) / 3
 	y := (t.A.Y + t.B.Y + t.C.Y) / 3
 	return Point{X: x, Y: y}
 }
 
+// Incenter calculates the incenter of the triangle
+func (t Triangle) Incenter() Point {
+	a := t.BC()
+	b := t.AC()
+	c := t.AB()
+	x := (a*t.A.X + b*t.B.X + c*t.C.X) / (a + b + c)
+	y := (a*t.A.Y + b*t.B.Y + c*t.C.Y) / (a + b + c)
+	return Point{X: x, Y: y}
+}
+
+// Circumcenter calculates the circumcenter of the triangle
+func (t Triangle) Circumcenter() Point {
+	D := 2 * (t.A.X*(t.B.Y-t.C.Y) + t.B.X*(t.C.Y-t.A.Y) + t.C.X*(t.A.Y-t.B.Y))
+	A2 := t.A.X*t.A.X + t.A.Y*t.A.Y
+	B2 := t.B.X*t.B.X + t.B.Y*t.B.Y
+	C2 := t.C.X*t.C.X + t.C.Y*t.C.Y
+	return Point{
+		X: (A2*(t.B.Y-t.C.Y) + B2*(t.C.Y-t.A.Y) + C2*(t.A.Y-t.B.Y)) / D,
+		Y: (A2*(t.C.X-t.B.X) + B2*(t.A.X-t.C.X) + C2*(t.B.X-t.A.X)) / D,
+	}
+}
+
+// Orthocenter calculates the orthocenter of the triangle
+func (t Triangle) Orthocenter() Point {
+	alpha, beta, gamma := t.Angles()
+	ta := math.Tan(alpha)
+	tb := math.Tan(beta)
+	tc := math.Tan(gamma)
+	return Point{
+		X: (ta*t.A.X + tb*t.B.X + tc*t.C.X) / (ta + tb + tc),
+		Y: (ta*t.A.Y + tb*t.B.Y + tc*t.C.Y) / (ta + tb + tc),
+	}
+}
+
+// NinePointCenter calculates the center point of the nine-point circle
+func (t Triangle) NinePointCenter() Point {
+	line := Line{
+		P: t.Circumcenter(),
+		Q: t.Orthocenter(),
+	}
+	return line.Midpoint()
+}
+
+// Incircle returns the incircle of the triangle
+func (t Triangle) Incircle() Circle {
+	return Circle{
+		Center: t.Incenter(),
+		Radius: t.IncircleRadius(),
+	}
+}
+
+// Circumcircle returns the circumcircle of the triangle
+func (t Triangle) Circumcircle() Circle {
+	return Circle{
+		Center: t.Circumcenter(),
+		Radius: t.CircumcircleRadius(),
+	}
+}
+
+// NinePointCircle returns the nine-point circle of the triangle
+func (t Triangle) NinePointCircle() Circle {
+	return Circle{
+		Center: t.NinePointCenter(),
+		Radius: t.NinePointRadius(),
+	}
+}
+
 // Copy returns a new triangle with the same vertices
-func (t *Triangle) Copy() Triangle {
+func (t Triangle) Copy() Triangle {
 	return Triangle{
 		A: t.A.Copy(),
 		B: t.B.Copy(),
@@ -765,7 +880,51 @@ func (t *Triangle) Copy() Triangle {
 }
 
 // Boundary returns the smallest rect that contains all three vertices
-func (t *Triangle) Boundary() Rect {
+func (t Triangle) Boundary() Rect {
 	curve := t.ToCurve()
 	return curve.Boundary()
+}
+
+// BarycentricSubdivision returns a slice of triangles that are the result of subdividing the triangle into three
+// smaller triangles using the centroid and each vertex as the new vertices
+func (t Triangle) BarycentricSubdivision() []Triangle {
+	result := make([]Triangle, 3)
+	centroid := t.Centroid()
+	result[0] = Triangle{A: t.A, B: t.B, C: centroid}
+	result[1] = Triangle{A: t.B, B: t.C, C: centroid}
+	result[2] = Triangle{A: t.C, B: t.A, C: centroid}
+	return result
+}
+
+// IncenterSubdivision returns a slice of triangles that are the result of subdividing the triangle into three
+// smaller triangles using the incenter and each vertex as the new vertices
+func (t Triangle) IncenterSubdivision() []Triangle {
+	result := make([]Triangle, 3)
+	incenter := t.Incenter()
+	result[0] = Triangle{A: t.A, B: t.B, C: incenter}
+	result[1] = Triangle{A: t.B, B: t.C, C: incenter}
+	result[2] = Triangle{A: t.C, B: t.A, C: incenter}
+	return result
+}
+
+// CircumcenterSubdivision returns a slice of triangles that are the result of subdividing the triangle into three
+// smaller triangles using the circumcenter and each vertex as the new vertices
+func (t Triangle) CircumcenterSubdivision() []Triangle {
+	result := make([]Triangle, 3)
+	circumcenter := t.Circumcenter()
+	result[0] = Triangle{A: t.A, B: t.B, C: circumcenter}
+	result[1] = Triangle{A: t.B, B: t.C, C: circumcenter}
+	result[2] = Triangle{A: t.C, B: t.A, C: circumcenter}
+	return result
+}
+
+// OrthocenterSubdivision returns a slice of triangles that are the result of subdividing the triangle into three
+
+func (t Triangle) OrthocenterSubdivision() []Triangle {
+	result := make([]Triangle, 3)
+	orthocenter := t.Orthocenter()
+	result[0] = Triangle{A: t.A, B: t.B, C: orthocenter}
+	result[1] = Triangle{A: t.B, B: t.C, C: orthocenter}
+	result[2] = Triangle{A: t.C, B: t.A, C: orthocenter}
+	return result
 }
